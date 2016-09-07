94ce5c24b650f25f081c1155e269c81ce8acf5a7
Salvatore D'Agostino <toto19@gmail.com>
2016-09-06 22:41:26 -0700

    update DB schema
    add UUID to all tables
    
    reference UUID instead of user_email

--- migrates/migrate.sql
+++ migrates/migrate.sql
@@ -4,42 +4,48 @@
  DROP TABLE IF EXISTS
 */
 
-DROP TABLE IF EXISTS "user";
 DROP TABLE IF EXISTS token;
 DROP TABLE IF EXISTS notification;
+DROP TABLE IF EXISTS "user";
 
 /**
  CREATE TABLE
 */
 CREATE TABLE "user"
 (
+id UUID NOT NULL UNIQUE ,
 email VARCHAR(255) NOT NULL UNIQUE ,
 password TEXT,
-PRIMARY KEY (email)
+PRIMARY KEY (id)
 );
 
 CREATE TABLE token
 (
+id UUID NOT NULL UNIQUE ,
 token VARCHAR(255) UNIQUE ,
-user_email VARCHAR(255),
-status BOOL DEFAULT true,
-PRIMARY KEY (token)
+user_id UUID,
+status BOOL DEFAULT TRUE,
+PRIMARY KEY (id)
 );
 
 CREATE TABLE notification
 (
 id UUID NOT NULL UNIQUE ,
 message TEXT,
-user_email VARCHAR(255),
+user_id UUID,
 PRIMARY KEY (id)
 );
-
 
 /**
  CREATE INDEX
  ADD FOREIGN KEY
 */
+CREATE INDEX user_id_idx ON "user"(id);
+
 CREATE INDEX user_email_idx ON "user"(email);
+
 CREATE INDEX token_idx ON token(token);
-ALTER TABLE token ADD FOREIGN KEY (user_email) REFERENCES "user" (email);
-ALTER TABLE notification ADD FOREIGN KEY (user_email) REFERENCES "user" (email);
+
+ALTER TABLE token ADD FOREIGN KEY (user_id) REFERENCES "user" (id);
+
+ALTER TABLE notification ADD FOREIGN KEY (user_id) REFERENCES "user" (id);
