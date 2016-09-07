2e4ef6bd5122ad6141535df21b2b18d360113b5d
Salvatore D'Agostino <toto19@gmail.com>
2016-09-06 22:41:02 -0700

    initial import of test db seeds

--- migrates/seeds.sql
+++ migrates/seeds.sql
@@ -1,1 +1,9 @@
 -- SQL SEEDS
+
+DELETE FROM notification;
+DELETE FROM token;
+DELETE FROM "user";
+
+INSERT INTO "user" (id, email, password) VALUES ('c93791f0-29d8-4e29-b944-ec2bc5692069', 'foo@example.com', 'password');
+INSERT INTO token (id, token, user_id) VALUES ('c14b368b-1467-461b-85bf-3feda8339f8e', 'atesttoken', 'c93791f0-29d8-4e29-b944-ec2bc5692069');
+INSERT INTO notification (id, message, user_id) VALUES ('409c4e13-10e5-4530-b35b-b57c01d01e9c', 'A Notification', 'c93791f0-29d8-4e29-b944-ec2bc5692069');
